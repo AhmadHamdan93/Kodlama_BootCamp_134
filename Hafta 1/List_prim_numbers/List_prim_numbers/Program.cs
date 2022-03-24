5f35e9f247690371012bd4eda62117ccaf7c57d0
Hamdan <ahmadkalo93@gmail.com>
2022-03-24 13:27:58 +0300

    list of prim numbers

---
+++ Hafta 1/List_prim_numbers/List_prim_numbers/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+for (int i = 1; i < 1000; i++)
+{
+    bool check = true;
+    for (int j = 2; j <= Math.Sqrt(i); j++)
+    {
+        if (i % j == 0)
+        {
+            check = false;
+        }
+    }
+    if (check) { Console.WriteLine($"the number {i} is prim"); }
+
+}
+
