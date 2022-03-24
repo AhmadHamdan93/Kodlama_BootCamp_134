7c68423a38b30b46d70de1209fa2e13b76474034
Hamdan <ahmadkalo93@gmail.com>
2022-03-24 13:04:10 +0300

    Delta with try_catch

--- Hafta 1/Delta/Delta/Program.cs
+++ Hafta 1/Delta/Delta/Program.cs
@@ -1,13 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+
 int a = 0;
 Console.WriteLine("a = ");
-a = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("b = ");
-int b = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("c = ");
-int c = Convert.ToInt32(Console.ReadLine());
+try {
+    a = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("b = ");
+    int b = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("c = ");
+    int c = Convert.ToInt32(Console.ReadLine());
+    int delta = b * b - 4 * c;
+    Console.WriteLine($"delta = {delta}");
+}
+catch(FormatException) {
+    Console.WriteLine("Please just Enter Number");
+}   
 
-int delta = b * b - 4 * c;
-Console.WriteLine($"delta = {delta}");
+
