b2ecd788a80d0fc4162d44684115515910f658f1
Hamdan <ahmadkalo93@gmail.com>
2022-03-24 12:59:44 +0300

    delta without try_catch

--- Hafta 1/Delta/Delta/Program.cs
+++ Hafta 1/Delta/Delta/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+int a = 0;
+Console.WriteLine("a = ");
+a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("c = ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int delta = b * b - 4 * c;
+Console.WriteLine($"delta = {delta}");
