ebcf5e4fe0a392405431bc2bdff990f1b2f88537
Hamdan <ahmadkalo93@gmail.com>
2022-03-24 13:17:08 +0300

    first edit prim numbers

---
+++ Hafta 1/Prim_Numbers/Prim_Numbers/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Enter Number :");
+int num = 0;
+num = Convert.ToInt32(Console.ReadLine());
+bool primChecked = true;
+for(int i = 2; i < num; i++)
+{
+    if(num % i == 0)
+    {
+        primChecked = false;
+    }
+}
+if(primChecked)
+    Console.WriteLine($"the number {num} is prim");
+else
+    Console.WriteLine($"the number {num} is not prim");
